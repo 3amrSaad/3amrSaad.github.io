e1073adecbfbe064324316cad40c6adb9ad898ed
3amrSaad <3amr.saad@gmail.com>
2020-11-20 14:27:00 +0200

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-be0e9d6ddc789fc3d27a.js"
+    "url": "webpack-runtime-d165488531522320afe4.js"
   },
   {
     "url": "framework-3321703c33570853116c.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "9fd0e1f7b313a4ee6b41f21aca04ff76"
+    "revision": "ef2daac2cd2e4c30ca49165294769893"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b1462017a2c34a95e2b4.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "4d3762611dc7fe23b30a1ccd51b3cd88"
+    "revision": "2c2090800fc8fef9eb63975d990f05ef"
   },
   {
     "url": "manifest.webmanifest",
