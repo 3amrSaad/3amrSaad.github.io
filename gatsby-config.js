cd2c46b1e43cb4ef2febf0d4c062f7af5cf8d9a9
Amr Saad <3amr.saad@gmail.com>
2020-11-20 16:16:36 +0200

    Updated Google Analytics TrackingID

--- gatsby-config.js
+++ gatsby-config.js
@@ -151,7 +151,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: 'UA-45666519-2',
+        trackingId: 'G-3BQTZWT40J',
       },
     },
   ],
