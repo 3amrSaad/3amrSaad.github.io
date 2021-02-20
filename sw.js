bf22679f6de80d14ab7ddf031abb53e1251b7155
3amrSaad <3amr.saad@gmail.com>
2021-02-20 22:54:08 +0200

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-0d94890b8069cb1f5653.js"
+    "url": "webpack-runtime-7aab8fbb0d0600684e5a.js"
   },
   {
     "url": "framework-3154d4619550b80a5138.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "b58f887ba710cee41d0bc621c6af0926"
+    "revision": "710c3215438a785b8547f851c0d419b2"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-cf5c1690ece9458e4830.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "8c9aad7172976b57474062ff52944c9b"
+    "revision": "ea4782aca7e469a2717c641c9ba2562c"
   },
   {
     "url": "polyfill-a87eb95ccd509b0cf58e.js"
